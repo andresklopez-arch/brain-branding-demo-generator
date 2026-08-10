af598c7bebbcadc76615792afc52ec448f6eec1e
Andre <andre@example.com>
2026-08-10 12:37:11 -0600

    Fix: Silence visit alerts and configure API_BASE for 2FA OTP

--- public/sw.js
+++ public/sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = 'bb-cache-v37.0.0-force-update';
+const CACHE_NAME = 'bb-cache-v38.0.0-force-update';
 const ASSETS = [
   '/',
-  '/index.html?v=37.0.0',
-  '/index.css?v=37.0.0',
-  '/app.js?v=37.0.0',
-  '/js/analytics.js?v=37.0.0',
+  '/index.html?v=38.0.0',
+  '/index.css?v=38.0.0',
+  '/app.js?v=38.0.0',
+  '/js/analytics.js?v=38.0.0',
   '/favicon.png'
 ];
 
